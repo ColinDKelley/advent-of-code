38539ad981111b68ffd293675fee6f6eaa50dd99
Colin Kelley <colindkelley@gmail.com>
2022-12-05 21:45:28 -0800

    2022 6 code golf

--- 2022/6/day6.rb
+++ 2022/6/day6.rb
@@ -1,19 +1,8 @@
-MIN_CHARS = 14
+def first_unique(chars, len)
+ (len..chars.size).find { chars[_1 - len, len].uniq.size == len }
+end
 
-index = 0
-buffer = []
+chars = STDIN.read.chars
 
-STDIN.each_char do |char|
-  buffer << char
-
-  if buffer.size > MIN_CHARS
-    buffer.shift
-  end
-
-  index += 1
-
-  if buffer.uniq.size == MIN_CHARS
-    puts(buffer.join, index)
-    break false
-  end
-end and puts("not found! #{buffer.join} #{index}")
+puts first_unique(chars, 4)
+puts first_unique(chars, 14)
