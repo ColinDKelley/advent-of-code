aee024d5992a52202313612d0c05608eb022e3a2
Colin Kelley <colindkelley@gmail.com>
2022-12-11 11:35:55 -0800

    2022 10 part 2

--- 2022/10/day10.rb
+++ 2022/10/day10.rb
@@ -1,17 +1,25 @@
 @x = 1
 @cycles = [@x]
+@pixels = []
+
+def sprite_visible?
+  delta = @x - (@cycles.size % 40)
+  (-2..0).include?(delta).tap { puts "cycle: [#{@cycles.size}]=#{@x} delta: #{delta}: #{_1}" }
+end
 
 def cycle
+  @pixels << (sprite_visible? ? '#' : ' ')
+  puts @pixels.each_slice(40).map(&:join)
   @cycles << @x
 end
 
 STDIN.each_line do |line|
   case line.split
   in ['noop']
-    @cycles << @x
+    cycle
   in ['addx', count]
-    @cycles << @x
-    @cycles << @x
+    cycle
+    cycle
     @x += count.to_i
   in []
   else
@@ -19,7 +27,7 @@ STDIN.each_line do |line|
   end
 end
 
-@cycles << @x
+cycle
 
 strength = @cycles.map.with_index(&:*)
 
