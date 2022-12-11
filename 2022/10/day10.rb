4033af171442e1a4436d436b4e4c3a5055f75feb
Colin Kelley <colindkelley@gmail.com>
2022-12-11 11:50:56 -0800

    2022 10 refactor

--- 2022/10/day10.rb
+++ 2022/10/day10.rb
@@ -1,34 +1,30 @@
 @x = 1
 @cycles = [@x]
-@pixels = []
 
-def sprite_visible?
-  delta = @x - (@cycles.size % 40)
-  (-2..0).include?(delta).tap { puts "cycle: [#{@cycles.size}]=#{@x} delta: #{delta}: #{_1}" }
+def sprite_visible?(cycle, x)
+  (-2..0).include?(x - (cycle % 40))
 end
 
-def cycle
-  @pixels << (sprite_visible? ? '#' : ' ')
-  puts @pixels.each_slice(40).map(&:join)
+def cycle!
   @cycles << @x
 end
 
 STDIN.each_line do |line|
   case line.split
-  in ['noop']
-    cycle
-  in ['addx', count]
-    cycle
-    cycle
+  in ["noop"]
+    cycle!
+  in ["addx", count]
+    cycle!
+    cycle!
     @x += count.to_i
-  in []
   else
     raise "unknown command #{line.split.inspect}"
   end
 end
 
-cycle
-
 strength = @cycles.map.with_index(&:*)
 
 puts [20, 60, 100, 140, 180, 220].map { |i| strength[i] }.sum
+
+pixels = @cycles.map.with_index { |x, cycle| sprite_visible?(cycle, x) ? "#" : " " }
+puts pixels[1..-1].each_slice(40).map(&:join)
